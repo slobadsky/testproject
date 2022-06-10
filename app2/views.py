38fd6786b17787b96a82afb356278d2734882f5b
slobadsky <mois.ibrahim@gmail.com>
2022-06-10 16:19:02 +0200

    redirect with user group is rectified

--- app2/views.py
+++ app2/views.py
@@ -3,54 +3,69 @@ from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.models import User
 import datetime
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # Create your views here.
 
 
-def not_in_student_group(user):
-    if user:
-        if user.groups.filter(name='group_app2').exists():
-            return True
-        else:
-            return False
-
-
 @login_required(login_url='app2-login-page',redirect_field_name=None)
-@user_passes_test(not_in_student_group, login_url='app2-login-page')
 def index(request):
-    print(request.user.groups.filter(name='group_app1').exists())
-    #print(request.user.groups.values_list('name',flat = True)[0])
-    return render(request,'app2/index.html',{
-            'active_page':'app2-index-page',
-            'test':datetime.datetime.now(),
-            'ops':list(request.user.groups.values_list('name',flat = True))
-            })
+    if request.user.groups.filter(name='group_app2').exists():
+        #print(request.user.groups.values_list('name',flat = True)[0])
+        return render(request,'app2/index.html',{
+                'active_page':'app2-index-page',
+                'test':datetime.datetime.now(),
+                'ops':list(request.user.groups.values_list('name',flat = True))
+                })
+    else:
+        return render(request,'app2/permission-req.html',{})
 
 def pr_page(request):
     return render(request,'app2/permission-req.html',{
             'test':datetime.datetime.now(),
             })
 
+
 def login_page(request):
-    if not request.user.is_authenticated:
-        if request.method == "POST":
-            uname =request.POST.get('username')
-            upass = request.POST.get('password')
-            user = authenticate(username=uname, password=upass)
-            if user is not None:                
-                if user.groups.values_list('name',flat = True)[0] == "group_app2":
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        #Manual code to Check whether username is valid or not
+        try:
+            myuser = User.objects.get(username=username)
+        except:
+            myuser = False
+
+        if myuser == False:
+            mypass = False
+        else:
+            mypass = myuser.check_password(password)
+        user = authenticate(username=username, password=password)
+        if myuser != False:
+            if user is not None:
+                if user.groups.values_list('name',flat = True)[0] == "group_app1":
+                    login(request,user)
+                    return HttpResponseRedirect('/app1')
+                elif user.groups.values_list('name',flat = True)[0] == "group_app2":
                     login(request,user)
                     return HttpResponseRedirect('/app2')
                 else:
-                    messages.error(request,"Unautorised group.")
-                    return HttpResponseRedirect('/app2/login')   
+                    pass
             else:
-                messages.error(request,"Invalid username or password.")
+                messages.error(request, "Le mot de passe incorrect")
                 return HttpResponseRedirect('/app2/login')
         else:
-            return render(request,'app2/login.html',{})
+            messages.error(request, "L'identifiant incorrect")
+            return HttpResponseRedirect('/app2/login')    
+        
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/app2')
     else:
-        return HttpResponseRedirect('/app2/login')
+        return render(request,'app2/login.html',{})
+
+    
+
 
 def logout_page(request):
     logout(request)
